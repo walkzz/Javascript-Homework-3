76645a7bda715f44ad502ddeb394414e746359f4
Pavel Toshinski <pavelce2000@hotmail.com>
2024-05-15 18:17:37 +0200

    Added for-of loop

--- exercise-2.js
+++ exercise-2.js
@@ -45,3 +45,10 @@ const person = {
 for(let key in person){
     console.log(`${key}: ${person[key]}`);
 }
+
+// g
+const colors = ["red", "green", "blue"];
+
+for (let color of colors) {
+  console.log(`${color.length} ${color}`);
+}
