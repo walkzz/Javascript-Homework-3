ddf2d1571827bc61fdfdbbc9c1ff60bf64bb6fbb
Pavel Toshinski <pavelce2000@hotmail.com>
2024-05-15 17:48:46 +0200

    Added exercise problem and 3 objects

---
+++ exercise-1.js
@@ -0,0 +1,25 @@
+// Задача 1.
+// Да се креира:
+
+// a. објект кој што опишува книга
+
+// б. објект кој што опишува автомобил
+
+// в. објект кој што опишува студент
+
+// Секој од овие објекти да имаат барем по еден вгнезден објект или низа.
+
+const book = {
+    title: "Lorem Ipsum",
+    author: "Pavel Toshinski",
+    genres: ["Programming", "Technology"];
+}
+const car = {
+
+}
+const student = {
+    
+}
+console.log(book);
+console.log(car);
+console.log(student);
