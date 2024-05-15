12937a314b7f55d7447dd202d818091d59ecf257
Pavel Toshinski <pavelce2000@hotmail.com>
2024-05-15 18:59:51 +0200

    Added exercise file #6 and exercise description, plus array of cities

---
+++ exercise-6.js
@@ -0,0 +1,20 @@
+// Задача 6.
+// Креирајте низа од 10 градови и испечатети ги во конзола со помош на:
+
+// a. for-in циклус
+
+// б. for-of циклус
+
+
+let cities = [
+"Tokyo", 
+"Paris", 
+"Sydney", 
+"Cairo", 
+"Toronto", 
+"Buenos", 
+"Aires", 
+"Reykjavik", 
+"Nairobi", 
+"Berlin", 
+"San Francisco"];
